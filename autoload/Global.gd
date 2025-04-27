08e9ef15b56b984c7d075c1285b1a38f80f5d7f2
Obscurite1221 <unlovedx2@gmail.com>
2025-04-27 02:47:32 -0700

    Added Descriptions
    Added a brief description to all the implemented algorithms. Still needs some formatting work.

--- autoload/Global.gd
+++ autoload/Global.gd
@@ -38,7 +38,8 @@ var algorithms = {
 			"min_value": 0.0,
 			"max_value": 5.0,
 			"default_value": 1.0,
-			}]
+			}],
+			"description": "Contrast modifies the RGB values of the image by multiplying them against a fixed value, maintaining their ratio so as to not disrupt the color balance"
 	},
 	"noise": {
 		"num_params": 2,
@@ -53,7 +54,8 @@ var algorithms = {
 			"min_value": 0.0,
 			"max_value": 1000.0,
 			"default_value": 0.0,
-		}]
+		}],
+		"description": "Adds random noise to the image based on the UV coordinates and the seed value"
 	},
 	"brightness": {
 		"num_params": 4,
@@ -80,7 +82,8 @@ var algorithms = {
 			"min_value": -1.0,
 			"max_value": 1.0,
 			"default_value": 0.0,
-			}]
+			}],
+			"description": "Brightness works in a similar manner to Contrast, where it modifies the RGB values by a constant amount. However, Brightness maintains the absolute difference between RBG values as opposed to the relative (Or proportional) difference"
 	},
 	"thresholding_rgb": {
 		"num_params": 3,
@@ -101,7 +104,8 @@ var algorithms = {
 			"min_value": 0.0,
 			"max_value": 100.0,
 			"default_value": 50.0
-		}]
+		}],
+		"description": "Thresholding allows you to map an RGB image into a binary form based on whether or not the individual color channels (Red, Green, and Blue) are above or below the threshold"
 	},
 	"thresholding": {
 		"num_params": 1,
@@ -110,11 +114,13 @@ var algorithms = {
 			"min_value": 0.0,
 			"max_value": 255.0,
 			"default_value": 255.0/2
-		}]
+		}],
+		"description": "Thresholding is an algorithm that converts an n-bit pixel into a one-bit activation map. I.e. black and white"
 	},
 	"image_negative": {
 		"num_params": 0,
-		"parameters": []
+		"parameters": [],
+		"description": "Image Negative inverts the color channel by subtracting the current color values by the maximum possible. I.e. 255 (8-bit) minus n"
 	},
 	"log_transform": {
 		"num_params": 1,
@@ -123,7 +129,8 @@ var algorithms = {
 			"min_value": 0.0,
 			"max_value": 2.0,
 			"default_value": 1.0
-		}]
+		}],
+		"description": "Log Transform takes the log^2 of the RGB values multiplied by a scaling constant. It notably enhances dark images",
 	},
 	"power_law": {
 		"num_params": 2,
@@ -138,7 +145,8 @@ var algorithms = {
 			"min_value": 0.0,
 			"max_value": 2.0,
 			"default_value": 1.0
-		}]
+		}],
+		"description": "The Power Transform is useful to brighten or darken the image in a non-linear fashion according to a given exponent and a scaling constant"
 	},
 	"contrast_stretching": {
 		"num_params": 4,
@@ -165,8 +173,30 @@ var algorithms = {
 			"min_value": 0.00,
 			"max_value": 255.00,
 			"default_value": 255.0
-		}
-		]
+		}],
+		"description": "Contrast Stretching applies a piecewise function to essentially compress or expand an RGB range, minimizing the values above and below the thresholds and stretching the color ranges inside the threshold to compensate for the removal of other colors"
+	},
+	"convolution": {
+		"num_params": 3,
+		"parameters": [{
+			"param": "radius",
+			"min_value": 1,
+			"max_value": 3,
+			"default_value": 1
+		},
+		{
+			"param": "horizontal_skew",
+			"min_value": 0.0,
+			"max_value": 2.0,
+			"default_value": 1.0
+		},
+		{
+			"param": "vertical_skew",
+			"min_value": 0.0,
+			"max_value": 2.0,
+			"default_value": 1.0
+		}],
+		"description": "Convolution is a fundamental technique that involves using a Kernel to compare nearby pixels to determine the value of the root pixel"
 	},
 	"unsharp_masking": {
 		"num_params": 1,
@@ -175,7 +205,8 @@ var algorithms = {
 			"min_value": 0.0, # Min value for the slider
 			"max_value": 8.0, # Max value for the slider
 			"default_value": 0.0, # Default value for the slider
-		}]
+		}],
+		"description": "Unsharp Masking creates a blurred version of the original image and uses that as a guide to sharpen the original image further based on how the blurred image compares to the original"
 	},
 	"highboost_filtering": {
 		"num_params": 1,
@@ -190,7 +221,8 @@ var algorithms = {
 			"min_value": 0.0, # Min value for the slider
 			"max_value": 5.0, # Max value for the slider
 			"default_value": 1.0, # Default value for the slider
-		}]
+		}],
+		"description": "Highboost filtering involves boosting the high-frequency components of an image without sacrificing the low-frequency components any more than necessary. It involves preserving and amplifying the high frequency components of the image (Edges, corners) and overwriting the low frequency parts of the image where necessary to boost image fidelity"
 	},
 }
 
